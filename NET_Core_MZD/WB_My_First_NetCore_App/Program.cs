a32d34b339d605dc75c0dbee4f872ba547215cda
María Luisa Zayas de Diego <MLZayas@kabel.es>
2019-09-12 08:50:35 +0200

    Adding White Belt tutorials and starting the first one of Yellow Belt

---
+++ NET_Core_MZD/WB_My_First_NetCore_App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WB_My_First_NetCore_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Kabel!");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ NET_Core_MZD/YB_Create_Web_Api/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YB_Create_Web_Api.Models;
+
+namespace YB_Create_Web_Api.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+                
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
