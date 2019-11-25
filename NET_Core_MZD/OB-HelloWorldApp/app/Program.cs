b8dad4b0f54bb8d389467fb6ae206fbd8c0cc27a
María Luisa Zayas de Diego <MLZayas@kabel.es>
2019-11-25 12:40:31 +0100

    Add HelloWorld App created with dotnet-cli

---
+++ NET_Core_MZD/OB-HelloWorldApp/app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myappmzd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
